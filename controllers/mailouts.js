f3ae2ce147d58c3d25848bb154cb8e9b4be8772a
Reagan Ibbotson <reagan_ib@hotmail.com>
2019-11-26 15:36:14 +1100

    Zipping now working, Word docs good

--- controllers/mailouts.js
+++ controllers/mailouts.js
@@ -9,8 +9,10 @@
 const knex = require('knex')
 const moment = require('moment')
 const JSZip = require('jszip')
-const officegen = require('officegen')
-const { Document, Packer, Paragraph, TextRun, ...docx } = require("docx")
+const { Document, Packer, Paragraph, TextRun, UnderlineType, HeadingLevel, AlignmentType, TabStopPosition } = require("docx")
+
+const path = require('path')
+const fs = require('fs')
 
 const config = require('../config')
 
@@ -21,9 +23,6 @@ const db = knex({
     ssl: true,
   }
 })
-
-const path = require('path')
-const fs = require('fs')
 
 const getFirstMonday = () => {
 	let thingo = moment().add(1, 'months').date(1).day(1).format('YYYY-MM-DD')
@@ -43,75 +42,6 @@ const getLastFriday = () => {
 	}
 
 	return thingo
-}
-
-const makeMailoutDoc = async (data) => {
-	const { venue_id, sub_date, type, text1, text2, text3, text4, text5, text6, filters } = data
-	const venueSignoff = await db.raw(`
-			select
-				boss,
-				boss_role,
-				venue_name
-			from venues
-			where venue_id = ${venue_id}
-		`)
-		.then(data => {
-			return [data.rows[0].boss, data.rows[0].boss_role, data.rows[0].venue_name]
-		})
-		.catch(err => {
-			console.log('Error getting venue signoff.', err)
-			return err
-		})
-		// Create the document object
-		let docx = officegen('docx')
-
-		docx.on('finalize', (written) => {
-			console.log('File created for: ', venue_id, type)
-		})
-
-		docx.on('error', (err) => {
-			console.log(err)
-		})
-		// Begin adding paragraphs and content to document object
-		let pObj = docx.createP()
-		pObj.addText(text1)
-
-		pObj = docx.createP()
-		pObj.addText('Kind regards,')
-
-		pObj = docx.createP()
-		venueSignoff.forEach(line=>{
-			pObj.addText(line)
-			pObj.addLineBreak()
-		})
-		// Loop 4 times to create weekly offers
-		for (x = 1; x <= 4; x++) {
-			pObj = docx.createP()
-			pObj.addText(`Offer ${x}`,{bold: true, font_size: 16})
-
-			pObj = docx.createP()
-			switch (x){
-				case 1:
-					pObj.addText(text2)
-					break
-				case 2:
-					pObj.addText(text3)
-					break
-				case 3:
-					pObj.addText(text4)
-					break
-				default:
-					pObj.addText(text5) 
-			}
-		}
-
-		pObj = docx.createP()
-		pObj.addText('Venue Designated Text Page', {bold: true, font_size: 16})
-
-		pObj = docx.createP()
-		pObj.addText(text6)
-
-	return docx
 }
 
 const makeBdayDoc = async (data) => {
@@ -297,67 +227,6 @@ module.exports = {
 
 
 
-
-const test = async () => {
-
-	const data = {
-		id: 123456789,
-		venue_id: 7322,
-		sub_date: '2019-12-01',
-		type: 'mailout', 
-		text1: 'You have the chance to win these weekly prizes in December: Week 1 & 3 prizes: A $250 Christmas Hamper will be drawn at 11am on Monday 9 and 23 December 2019. Week 2 & 4 prizes: A $200 Bunnings Gift Card will be drawn at 11am on Monday 16 and 30 December 2019. Remember to bring in your four weekly entry coupons when visiting the venue to enter into each weekly prize draw for your chance to win. We look forward to seeing you soon. ', 
-		text2: 'Present this voucher to receive a complimentary drink (Glass of house wine or glass of soft drink). Only valid at Abruzzo Club between 1 to 8 December 2019. Not valid with any other offer.',
-		text3: 'Present this voucher to receive a complimentary drink (Glass of house wine or glass of soft drink). Only valid at Abruzzo Club between 9 to 15 December 2019. Not valid with any other offer.',
-		text4: 'Present this voucher to receive a complimentary drink (Glass of house wine or glass of soft drink). Only valid at Abruzzo Club between 16 to 22 December 2019. Not valid with any other offer.',
-		text5: 'Present this voucher to receive a complimentary drink (Glass of house wine or glass of soft drink). Only valid at Abruzzo Club between 23 to 29 December 2019. Not valid with any other offer.',
-		text6: 'heasefay',
-		filters: []
-	}
-
-	const bdayData = {
-		id: 123456790,
-		venue_id: 7322,
-		sub_date: '2019-12-01',
-		type: 'bday',
-		text1: 'Wishing you a very Happy Birthday and a wonderful year ahead. To help celebrate your Birthday, please find attached two special offers for you to use throughout the month. We look forward to seeing you soon.', 
-		text2: 'Present this voucher to receive a complimentary Drink (Cascade Light or VB or glass of house wine). Only valid at Abruzzo Club between 1 to 15 December 2019. Not valid with any other offer.',
-		text3: 'Present this voucher to receive a complimentary Drink (Cascade Light or VB or glass of house wine). Only valid at Abruzzo Club between 16 to 31 December 2019. Not valid with any other offer.',
-		text4: 'text 4',
-		text5: 'text 5',
-		text6: 'text 6',
-		filters: []
-	}
-
-	const zip = new JSZip()
-
-	const mailoutStuff = await makeMailoutDoc(data)
-	let out = fs.createWriteStream(path.join(__dirname, 'example-mailout.docx'))
-	out.on('error', (err) => {
-		console.log(err)
-	})
-	await mailoutStuff.generate(out)
-
-	zip.file('example-mailout.docx')
-
-	const bDayStuff = await makeBdayDoc(bdayData)
-	let bdayOut = fs.createWriteStream(path.join(__dirname, 'example-bday-mailout.docx'))
-	bdayOut.on('error', (err) => {
-		console.log(err)
-	})
-	await bDayStuff.generate(bdayOut)
-
-	zip.file('example-bday-mailout.docx')
-
-	zip.generateAsync({type: 'nodebuffer'})
-		.then((content) => {
-			fs.writeFileSync(path.join(__dirname, 'testzip.zip'), content)
-		})
-
-
-	return
-}
-
-
 const docxMakeMailout = async (data) => {
 	const { venue_id, sub_date, type, text1, text2, text3, text4, text5, text6, filters } = data
 	const venueSignoff = await db.raw(`
@@ -376,52 +245,369 @@ const docxMakeMailout = async (data) => {
 			return err
 		})
 
-	const doc = new Document()
+	const doc = new Document({
+		styles: {
+			paragraphStyles: [
+				{
+					id: "Heading1",
+					name: "Heading 1",
+					basedOn: "Normal",
+					next: "Normal",
+					quickFormat: true,
+					run: {
+						font: "Calibri",
+						size: 26,
+						bold: true,
+						color: "000000",
+						underline: {
+							type: UnderlineType.SINGLE,
+							color: "000000",
+						},
+					},
+					paragraph: {
+						alignment: AlignmentType.LEFT,
+						spacing: {
+							line: 340
+						},
+					},
+				},
+				{
+					id: "normalPara",
+					name: "Normal Para",
+					basedOn: "Normal",
+					next: "Normal",
+					quickFormat: true,
+					run: {
+						font: "Calibri",
+						size: 22,
+						bold: false,
+					},
+					paragraph: {
+						spacing: {
+							line: 276, 
+							before: 20 * 72 * 0.1, 
+							after: 20 * 72 * 0.05
+						},
+						rightTabStop: TabStopPosition.MAX,
+						leftTabStop: 453.543307087,
+					},
+				},
+				{
+					id: "wellSpaced",
+					name: "Well Spaced",
+					basedOn: "Normal",
+					run: {
+						font: "Calibri",
+						size: 22,
+						bold: false,
+					},
+					paragraph: {
+						spacing: {
+							line: 240,
+							before: 20 * 72 * 0.1,
+							after: 20 * 72 * 0.1
+						},
+					},
+				},
+				{
+					id: "wellSpacedAfter",
+					name: "Well Spaced After",
+					basedOn: "Normal",
+					run: {
+						font: "Calibri",
+						size: 22,
+						bold: false,
+					},
+					paragraph: {
+						spacing: {
+							line: 240,
+							after: 20 * 72 * 0.1
+						},
+					},
+				},
+				{
+					id: "wellSpacedBefore",
+					name: "Well Spaced Before",
+					basedOn: "Normal",
+					run: {
+						font: "Calibri",
+						size: 22,
+						bold: false,
+					},
+					paragraph: {
+						spacing: {
+							line: 240,
+							before: 20 * 72 * 0.1
+						},
+					},
+				},
+				{
+					id: "noSpaced",
+					name: "No Spaced",
+					basedOn: "Normal",
+					run: {
+						font: "Calibri",
+						size: 22,
+						bold: false,
+					},
+					paragraph: {
+						spacing: {
+							line: 240
+						},
+					},
+				},
+			],
+		},
+	})
 	
-	const myStyles = new docx.Styles()
-
-	myStyles.createParagraphStyle('myParaStyle','thingo')
-		.basedOn('Normal')
-		.next('Normal')
-		.spacing({line: 276})
-		.font('Arial')
-		.size(11)
-		.spacing({before: 100, after: 100})
-
 	doc.addSection({
 		properties: {},
 		children: [
 			new Paragraph({
-				text: text1
+				text: text1,
+				style: 'wellSpaced'
 			}),
 			new Paragraph({
-				text: 'Kind regards,'
+				text: 'Kind regards,',
+				style: 'wellSpaced'
 			}),
 			new Paragraph({
-				children: [
-					new TextRun(venueSignoff[0])
-				]
+				text: venueSignoff[0],
+				style: 'wellSpacedBefore'
 			}),
 			new Paragraph({
-				children: [
-					new TextRun(venueSignoff[1])
-				]
-			}).style('myParaStyle'),
+				text: venueSignoff[1],
+				style: 'noSpaced'
+			}),
 			new Paragraph({
-				children: [
-					new TextRun(venueSignoff[2])
-				]
-			}).style('myParaStyle'),
+				text: venueSignoff[2],
+				style: 'wellSpacedAfter'
+			}),
+			new Paragraph({
+				text: 'Offer 1',
+				style: 'Heading1'
+			}),
+			new Paragraph({
+				text: text2,
+				style: 'wellSpaced'
+			}),
+			new Paragraph({
+				text: 'Offer 2',
+				style: 'Heading1'
+			}),
+			new Paragraph({
+				text: text3,
+				style: 'wellSpaced'
+			}),
+			new Paragraph({
+				text: 'Offer 3',
+				style: 'Heading1'
+			}),
+			new Paragraph({
+				text: text4,
+				style: 'wellSpaced'
+			}),
+			new Paragraph({
+				text: 'Offer 4',
+				style: 'Heading1'
+			}),
+			new Paragraph({
+				text: text5,
+				style: 'wellSpaced'
+			}),
+			new Paragraph({
+				text: 'Venue Designated Page',
+				style: 'Heading1'
+			}),
+			new Paragraph({
+				text: text6,
+				style: 'wellSpaced'
+			})
 		]
 	})
 
 
-	return await Packer.toBuffer(doc)//.then(buffer=>{return buffer})
+	return await Packer.toBuffer(doc)
 
 }
 
-const docxMakeBday = async () => {
+const docxMakeBday = async (data) => {
+	const { venue_id, sub_date, type, text1, text2, text3, text4, text5, text6, filters } = data
+	const venueSignoff = await db.raw(`
+			select
+				boss,
+				boss_role,
+				venue_name
+			from venues
+			where venue_id = ${venue_id}
+		`)
+		.then(data => {
+			return [data.rows[0].boss, data.rows[0].boss_role, data.rows[0].venue_name]
+		})
+		.catch(err => {
+			console.log('Error getting venue signoff.', err)
+			return err
+		})
+
+	const doc = new Document({
+		styles: {
+			paragraphStyles: [
+				{
+					id: "Heading1",
+					name: "Heading 1",
+					basedOn: "Normal",
+					next: "Normal",
+					quickFormat: true,
+					run: {
+						font: "Calibri",
+						size: 26,
+						bold: true,
+						color: "000000",
+						underline: {
+							type: UnderlineType.SINGLE,
+							color: "000000",
+						},
+					},
+					paragraph: {
+						alignment: AlignmentType.LEFT,
+						spacing: {
+							line: 340
+						},
+					},
+				},
+				{
+					id: "normalPara",
+					name: "Normal Para",
+					basedOn: "Normal",
+					next: "Normal",
+					quickFormat: true,
+					run: {
+						font: "Calibri",
+						size: 22,
+						bold: false,
+					},
+					paragraph: {
+						spacing: {
+							line: 276, 
+							before: 20 * 72 * 0.1, 
+							after: 20 * 72 * 0.05
+						},
+						rightTabStop: TabStopPosition.MAX,
+						leftTabStop: 453.543307087,
+					},
+				},
+				{
+					id: "wellSpaced",
+					name: "Well Spaced",
+					basedOn: "Normal",
+					run: {
+						font: "Calibri",
+						size: 22,
+						bold: false,
+					},
+					paragraph: {
+						spacing: {
+							line: 240,
+							before: 20 * 72 * 0.1,
+							after: 20 * 72 * 0.1
+						},
+					},
+				},
+				{
+					id: "wellSpacedAfter",
+					name: "Well Spaced After",
+					basedOn: "Normal",
+					run: {
+						font: "Calibri",
+						size: 22,
+						bold: false,
+					},
+					paragraph: {
+						spacing: {
+							line: 240,
+							after: 20 * 72 * 0.1
+						},
+					},
+				},
+				{
+					id: "wellSpacedBefore",
+					name: "Well Spaced Before",
+					basedOn: "Normal",
+					run: {
+						font: "Calibri",
+						size: 22,
+						bold: false,
+					},
+					paragraph: {
+						spacing: {
+							line: 240,
+							before: 20 * 72 * 0.1
+						},
+					},
+				},
+				{
+					id: "noSpaced",
+					name: "No Spaced",
+					basedOn: "Normal",
+					run: {
+						font: "Calibri",
+						size: 22,
+						bold: false,
+					},
+					paragraph: {
+						spacing: {
+							line: 240
+						},
+					},
+				},
+			],
+		},
+	})
 	
+	doc.addSection({
+		properties: {},
+		children: [
+			new Paragraph({
+				text: text1,
+				style: 'wellSpaced'
+			}),
+			new Paragraph({
+				text: 'Kind regards,',
+				style: 'wellSpaced'
+			}),
+			new Paragraph({
+				text: venueSignoff[0],
+				style: 'wellSpacedBefore'
+			}),
+			new Paragraph({
+				text: venueSignoff[1],
+				style: 'noSpaced'
+			}),
+			new Paragraph({
+				text: venueSignoff[2],
+				style: 'wellSpacedAfter'
+			}),
+			new Paragraph({
+				text: 'Offer 1',
+				style: 'Heading1'
+			}),
+			new Paragraph({
+				text: text2,
+				style: 'wellSpaced'
+			}),
+			new Paragraph({
+				text: 'Offer 2',
+				style: 'Heading1'
+			}),
+			new Paragraph({
+				text: text3,
+				style: 'wellSpaced'
+			})
+		]
+	})
+
+
+	return await Packer.toBuffer(doc)
 }
 
 
@@ -456,8 +642,21 @@ const test2 = async () => {
 	}
 
 	const testMailoutData = await docxMakeMailout(data)
+	const testBdayData = await docxMakeBday(bdayData)
 
 	fs.writeFileSync(path.join(__dirname,'examplemailoutdoco.docx'), testMailoutData)
+
+	const zip = new JSZip()
+
+	zip.file('mailouts/testzipfile1.docx', testMailoutData)
+	zip.file('mailouts/testzipfile2.txt', 'Hello hello')
+	zip.file('mailouts/testzipfile3.docx', testMailoutData)
+	zip.file('bdays/testbdayzipfile1.docx', testBdayData)
+
+	await zip.generateAsync({type: 'nodebuffer'})
+		.then((content) => {
+			fs.writeFileSync(path.join(__dirname, 'test2zip.zip'), content)
+		})
 	
 	process.exit()
 	return
